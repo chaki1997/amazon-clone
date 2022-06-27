daf0358633a54290a225406cb534b8707cbc4e09
chaki1997 <davit.chaki.97@gmail.com>
2022-06-27 20:27:47 +0400

    add Home page banner

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import './Home.css'
 function Home() {
   return (
-    <div>
-       <h1>I am a Home Page</h1>
+    <div className='home'>
+        <img
+            className='home__images'
+            src='./61BvxKSpy3L._SX3000_.jpg' alt='home_banner' />
+ 
     </div>
   )
 }
